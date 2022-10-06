b149cf2632f13a57cb3027e9524c788e05cf7b9f
Vlad <53918010+VladimirUE4@users.noreply.github.com>
2022-10-06 22:42:22 +0200

    Login System pre alpha

--- server/main.js
+++ server/main.js
@@ -9,10 +9,40 @@ onNet("js:debug", (msg) => {
 })
 
 
-onNet("db:test", () => {
-    const identifierddd = GetPlayerIdentifier(source, 0);
-    const name = "vlad";
-    const surname = "kusva";
-    const sql = "INSERT INTO users (identifier, firstname, lastname) VALUES ("+identifierddd+ ", "+name+", "+surname+")";
+function registerplayer() {
+    const identifiers = GetPlayerIdentifier(source, 0);
+    const sql = "INSERT INTO users (identifier, name, surname) VALUES ('"+identifiers+"', 'salut','lesmecs')";
     oxmysql.insert(sql)
+    console.log(identifiers, 'salut','lesmecs')
+};
+
+
+
+onNet("db:creer", () =>
+{
+    const identifiers = GetPlayerIdentifier(source, 0);
+    const sql = "INSERT INTO users (identifier, name, surname) VALUES ('"+identifiers+"', 'salut','lesmecs')";
+    oxmysql.insert(sql)
+    console.log(identifiers, 'salut','lesmecs')
 });
+
+
+
+
+
+onNet("db:isnew", () =>
+{
+    const identifiers = GetPlayerIdentifier(source, 0);
+    const sql = "SELECT * FROM `users` WHERE `identifier`='+identifiers+'";
+    oxmysql.query(sql, (result) => {
+        if(result) {
+            console.log("tg");
+        } else {
+            console.log('tg');
+            registerplayer();
+        }
+    });
+});
+
+
+
