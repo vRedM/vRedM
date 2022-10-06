dc9bdf2094550243a32132c0f5e75d111981ab92
Vlad <53918010+VladimirUE4@users.noreply.github.com>
2022-10-06 22:43:12 +0200

    Update player.js

--- client/modules/player.js
+++ client/modules/player.js
@@ -2,8 +2,16 @@ Wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 
 RegisterCommand("register", (source, args) => {
+    emitNet("js:debug", "Checking if you are already registered !");
+    emitNet("db:isnew");
+    return;
+    
+});
+
+
+RegisterCommand("creer", (source, args) => {
     emitNet("js:debug", "Hello from the client!");
-    emitNet("db:test");
+    emitNet("db:creer");
     return;
     
 });
